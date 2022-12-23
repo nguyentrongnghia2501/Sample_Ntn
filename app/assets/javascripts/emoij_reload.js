e447bc1a5758295a78a46cdad7c7ee5f83861763
nguyentrongnghia2501 <92906182+nguyentrongnghia2501@users.noreply.github.com>
2022-12-23 15:27:20 +0700

    ind4

--- app/assets/javascripts/emoij_reload.js
+++ app/assets/javascripts/emoij_reload.js
@@ -1,6 +1,5 @@
-
-$(document).ready(function(){
-    $("a.emoji-link").click( function(){
+$(document).ready(function() {
+    $("a.emoji-link").click(function() {
         var url = this.href;
         $("body").load(url);
         return false;
